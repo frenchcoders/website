5c7f6e863564b4ac5e70eb5880031619391cfc55
Thomas LHTS <contact@thts.fr>
2017-02-04 20:39:03 +0100

    Controleur princpal de la platforme.

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+# * * * * * * * * * * * * * * * * * * * *
+# CONTROLEUR PRINCIPAL DE LA PLATEFORME
+# * * * * * * * * * * * * * * * * * * * *
+include('config.php');
+if(isset($_GET['module'])):
+
+	if(is_dir(MODULES_DIR_PATH . $_GET['module'])):
+
+		# * * * * * * * * * * * * * * * * * * * * * * * *
+		# INCLURE LES MODELES ET LE CONTROLEUR SECONDAIRE
+		# * * * * * * * * * * * * * * * * * * * * * * * *
+
+		# Modèles
+		foreach($_Modules->incModels($_GET['module']) as $v):
+			include($v);
+		endforeach;
+		# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+		# Controleur secondaire
+		include($_Modules->incCtrl($_GET['module']));
+		# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+		# * * * * * * * * *
+		# ENTREES DE LANGUE
+		# * * * * * * * * *
+		$_Lang->load(MODULES_DIR_PATH . 'lang/');
+
+	# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+	else:
+
+		# * * * * * * * * * * * * * * * *
+		# LE MODULE N'A PAS ETRE TROUVE
+		# * * * * * * * * * * * * * * * *
+		throw new Exception('Can\'t find module ' . $_GET['module']);
+
+	endif;
+
+endif;
+?>
