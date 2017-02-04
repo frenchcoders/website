46a06fa02ef32c41cf79109aa0aa19344283d95c
Thomas LHTS <contact@thts.fr>
2017-02-04 20:40:23 +0100

    Templates généraux (header, head-logo, footer) de la plateforme.

---
+++ static/templates/client-header.php
@@ -0,0 +1,14 @@
+<!--
+	* * * * * * * * * * * * * * * * * * * * *
+	HEADER DES PAGES CLIENT DE LA PLATEFORME.
+	* * * * * * * * * * * * * * * * * * * * * -->
+
+<!-- Headbar -->
+<div class="col-lg-12" id="navbar">
+
+</div>
+
+<!-- Header -->
+<div class="col-lg-12" id="header">
+
+</div>
