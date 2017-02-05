260b2ba08f20714653e8b124ed02be0ab53ad31f
Thomas LHTS <contact@thts.fr>
2017-02-05 10:32:26 +0100

    Homepage ctrl : Composition du template.

---
+++ modules/home/controller.php
@@ -0,0 +1,18 @@
+<?php
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# CONTROLEUR SECONDAIRE DE LA HOMEPAGE " FRENCHCODERS "
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+use FrenchCoders\Core\Templates;
+
+define('MODULE_NAME', 'home');
+# - - - - - - - - - - - - - - - - - - - - - -
+
+$Template = new Templates(['static/templates/header.tpl.php',
+						  'modules/' . MODULE_NAME . '/templates/homepage.tpl.php',
+					  	  'static/templates/footer.tpl.php']);
+
+
+
+
+$Template->render();
+?>
