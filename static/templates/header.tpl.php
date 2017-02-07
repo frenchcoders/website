c15b0ab9bfa4e636b79286599d472318ebcf8280
Thomas LHTS <contact@thts.fr>
2017-02-07 14:59:51 +0100

    Header commun aux pages clients de la plateforme.

--- static/templates/header.tpl.php
+++ static/templates/header.tpl.php
@@ -6,17 +6,89 @@ HEADER COMMUN A LA PLATEFORME
 
 	<head>
 
+		<meta charset="utf-8" />
+		<title>#FrenchCoders</title>
+
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
 		<!-- BOOTSTRAP -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
 			  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-		<!-- = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = -->
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 
 		<!-- JQUERY-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+		<!-- CSS RULES -->
+		<link href="static/css/rules.css" rel="stylesheet" />
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+		<!-- ROBOTO -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,400,500,700" rel="stylesheet">
 
 	</head>
+	<!-- = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = -->
 
 	<body>
+
+		<div class="container-fluid">
+
+			<!-- Headbar -->
+			<div class="row">
+				<div class="col-lg-12" id="headbar">
+					Connecté en tant que <a class="fc-red">Thomas</a>
+
+					<span class="social-icons">
+
+						<!-- FACEBOOK -->
+						<a href="#">
+							<img src="static/imgs/facebook-icon.png" />
+						</a>
+						<!-- - - - - - - - - - - - - - - - - - - - - - - - - -->
+						<!-- TWITTER -->
+						<a href="#">
+							<img src="static/imgs/twitter-icon.png" />
+						</a>
+
+					</span>
+				</div>
+			</div>
+			<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+			<!-- Header -->
+			<div class="row">
+				<div class="col-lg-12" id="header">
+
+					<!-- Logo -->
+					<div id="logo">
+						<img src="static/imgs/logo.png" />
+					</div>
+
+					<div id="head-menu">
+						<ul>
+							<li>
+								<a href="#">Publications</a>
+								<span>Retrouvez les dernières publications ajoutées
+								sur la plateforme et restez informés des nouveautées de l'univers IT !</span>
+							</li>
+
+							<li>
+								<a href="#">Cours</a>
+								<span>Vous souhaitez apprendre à programmer ? N'hésitez plus, c'est gratuit !</span>
+							</li>
+						</ul>
+
+						<ul>
+							<li>
+								<a href="#">Communautée</a>
+								<span>Une question, un problème ? Demandez de l'aide à la communautée !</span>
+							</li>
+						</ul>
+
+					</div>
+
+				</div>
