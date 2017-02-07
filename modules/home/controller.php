83be391593913d0d49a6f1d3589e59c39117143b
Thomas LHTS <contact@thts.fr>
2017-02-07 14:57:02 +0100

    Ebauche d'un controleur secondaire pour le module Homepage

--- modules/home/controller.php
+++ modules/home/controller.php
@@ -4,15 +4,17 @@
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 use FrenchCoders\Core\Templates;
 
+# * * * * * * *
+# CONFIGURATION
+# * * * * * * *
+
 define('MODULE_NAME', 'home');
-# - - - - - - - - - - - - - - - - - - - - - -
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
 
 $Template = new Templates(['static/templates/header.tpl.php',
 						  'modules/' . MODULE_NAME . '/templates/homepage.tpl.php',
 					  	  'static/templates/footer.tpl.php']);
-
-
-
 
 $Template->render();
 ?>
