c1446b76a7980c7610f967d48fff9d6a42a5b036
Thomas LHTS <contact@thts.fr>
2017-02-07 14:57:35 +0100

    DOM du module Homepage

--- modules/home/templates/homepage.tpl.php
+++ modules/home/templates/homepage.tpl.php
@@ -1,7 +1,41 @@
+<!--
+* * * * * * * * * * * *
+HOMEPAGE - PRESENTATION
+* * * * * * * * * * * * -->
 <div class="row">
-    <div class="col-lg-12">
-		<div class="page-header">
-			<h1>Homepage</h1>
+    <div class="col-lg-12" id="presentation">
+		<div class="col-lg-5">
+			<h3>Bienvenue sur #FrenchCoders !</h3>
+			<div class="title-underline-white"></div>
+
+			<p><span class="medium">#FrenchCoders</span> est une communauté francophone de
+				<span class="medium">programmeurs</span> amateurs ou professionels
+				réunis autour d'un même centre d'interêt : La programmation informatique.</p>
+
+			<p>Promouvoir la <span class="medium">programmation</span> et le
+			<span class="medium">logiciel libre</span> en fournissant à ses utilisateurs
+			des <span class="medium">articles et cours gratuits</span>, clairs, complets
+			et de qualité et propulser une communauté active sont la motivation et l'essence
+			même de <span class="medium">#FrenchCoders</span>.</p>
 		</div>
-    </div>
+	</div>
+</div>
+<!-- = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = -->
+
+
+<!--
+* * * * * * * * * *
+HOMEPAGE - RECENTS
+* * * * * * * * * * -->
+<div class="row">
+	<div class="col-lg-12" id="recent">
+		<h3 class="recent-title">En ce moment sur #FrenchCoders</h3>
+		<div class="title-underline-black"></div>
+
+		<ul class="nav nav-tabs">
+		  <li class="active"><a href="#">Publications</a></li>
+		  <li><a href="#">Cours</a></li>
+		  <li><a href="#">Communauté</a></li>
+		</ul>
+	</div>
 </div>
